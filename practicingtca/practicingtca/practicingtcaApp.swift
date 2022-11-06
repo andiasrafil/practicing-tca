a826b6049302dbdddc3cc568b2075f5fd0389373
andiasrafil <41411347+andiasrafil@users.noreply.github.com>
2022-11-06 21:46:31 +0700

    init swiftui project

---
+++ practicingtca/practicingtca/practicingtcaApp.swift
@@ -0,0 +1,17 @@
+//
+//  practicingtcaApp.swift
+//  practicingtca
+//
+//  Created by TI Digital on 06/11/22.
+//
+
+import SwiftUI
+
+@main
+struct practicingtcaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
